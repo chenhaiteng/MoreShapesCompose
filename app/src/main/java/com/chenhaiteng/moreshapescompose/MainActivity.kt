4fa0405127775e4559496bf4919979b6fb0be6ba
Chen-Hai Teng <chenhaiteng@gmail.com>
2025-01-01 19:36:59 +0800

    Add example in test app

--- app/src/main/java/com/chenhaiteng/moreshapescompose/MainActivity.kt
+++ app/src/main/java/com/chenhaiteng/moreshapescompose/MainActivity.kt
@@ -1,16 +1,31 @@
 package com.chenhaiteng.moreshapescompose
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.chenhaiteng.moreshapes.CustomShapeView
+import com.chenhaiteng.moreshapes.KilaWheel
+import com.chenhaiteng.moreshapes.Lotus
+import com.chenhaiteng.moreshapes.RectangleLotus
+import com.chenhaiteng.moreshapes.kilaWheel
 import com.chenhaiteng.moreshapescompose.ui.theme.MoreShapesComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,16 +47,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column {
+        CustomShapeView(Modifier.size(150.dp), Lotus(), color = Color.Red)
+        CustomShapeView(Modifier.size(150.dp), KilaWheel(), color = Color.Blue)
+        CustomShapeView(Modifier.size(150.dp), RectangleLotus(), color = Color.Green)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MoreShapesComposeTheme {
-        Greeting("Android")
+        Greeting("Android", Modifier.fillMaxSize())
     }
 }
