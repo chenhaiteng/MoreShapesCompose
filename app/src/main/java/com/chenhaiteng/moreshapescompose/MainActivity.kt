0dc96c1ec8cf5a0b209c2a639614967d8c6158c4
Chen-Hai Teng <chenhaiteng@gmail.com>
2025-01-01 19:40:04 +0800

    clean import

--- app/src/main/java/com/chenhaiteng/moreshapescompose/MainActivity.kt
+++ app/src/main/java/com/chenhaiteng/moreshapescompose/MainActivity.kt
@@ -1,21 +1,14 @@
 package com.chenhaiteng.moreshapescompose
 
-import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredHeight
-import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -25,7 +18,6 @@ import com.chenhaiteng.moreshapes.CustomShapeView
 import com.chenhaiteng.moreshapes.KilaWheel
 import com.chenhaiteng.moreshapes.Lotus
 import com.chenhaiteng.moreshapes.RectangleLotus
-import com.chenhaiteng.moreshapes.kilaWheel
 import com.chenhaiteng.moreshapescompose.ui.theme.MoreShapesComposeTheme
 
 class MainActivity : ComponentActivity() {
